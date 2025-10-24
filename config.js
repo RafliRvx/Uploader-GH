7046b3cfd5f89bf11797370349e05e529c6b4e7b
RafliRvx <sigitbahtarun123@gmail.com>
2025-10-25 00:24:24 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+export const config = {
+    githubToken: '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', // Ganti dengan token GitHub Anda
+    owner: 'RafliRvx', // Ganti dengan username GitHub Anda
+    branch: 'main',
+    repos: ['dat1', 'dat2', 'dat3', 'dat4']
+};
